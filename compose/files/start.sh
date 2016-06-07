1d476d722eff9d34a9a0ce7b254f06b4920dfd72
thefosk <marco@mashape.com>
2016-06-06 22:26:24 -0700

    wip

--- compose/files/start.sh
+++ compose/files/start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 service nginx start
 consul-template -consul=$CONSUL_URL -template="/templates/default.ctmpl:/etc/nginx/conf.d/default.conf:service nginx reload"
+cat /etc/nginx/conf.d/default.conf
