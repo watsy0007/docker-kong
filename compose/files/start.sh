f0d7e00751eeec45a75cca80d522bc50792210ef
thefosk <marco@mashape.com>
2016-06-28 23:33:46 -0700

    Fixing consul template

--- compose/files/start.sh
+++ compose/files/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 service nginx start
-consul-template -consul=$CONSUL_URL -template="/templates/default.ctmpl:/etc/nginx/conf.d/default.conf:service nginx reload"
+consul-template -consul=$CONSUL_URL -template="/templates/default.ctmpl:/etc/nginx/nginx.conf:service nginx reload"
 cat /etc/nginx/conf.d/default.conf
