154d7126d91d914f23ded1b862635344fa89c4f3
watsy0007 <watsy0007@gmail.com>
2016-07-05 11:04:43 +0800

    update etc config

--- setup.sh
+++ setup.sh
@@ -1,30 +1,29 @@
 #!/bin/sh
 
 # Setting up the proper database
-path="/etc/kong/kong.yml"
 if [ -n "$DB_HOST" ]; then
     reg="1,2s/kong-database/${DB_HOST}/"
-    echo `(sed -i '' $reg $path)`
+    echo `(sed -i '' $reg /etc/kong/kong.yml)`
 fi
 
 if [ -n "$DB_PORT" ]; then
     reg="3,3s/5432/${DB_PORT}/"
-    echo `(sed -i '' $reg $path)`
+    echo `(sed -i '' $reg /etc/kong/kong.yml)`
 fi
 
 if [ -n "$DB_NAME" ]; then
     reg="4,4s/kong/${DB_USER}/"
-    echo `(sed -i '' $reg $path)`
+    echo `(sed -i '' $reg /etc/kong/kong.yml)`
 fi
 
 if [ -n "$DB_USER" ]; then
     reg="5,5s/kong/${DB_USER}/"
-    echo `(sed -i '' $reg $path)`
+    echo `(sed -i '' $reg /etc/kong/kong.yml)`
 fi
 
 if [ -n "$DB_PASS" ]; then
     reg="6,6s/kong/${DB_PASS}/"
-    echo `(sed -i '' $reg $path)`
+    echo `(sed -i '' $reg /etc/kong/kong.yml)`
 fi
 
 if [ -n "$DATABASE" ]; then
